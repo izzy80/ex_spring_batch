6ca4ba46c741cee557147e3f0e39d0dbbde982ea
izzy80 <115052929+izzy80@users.noreply.github.com>
2025-01-12 15:32:24 +0900

    git init

---
+++ src/main/java/com/study/ex_spring_batch/ExSpringBatchApplication.java
@@ -0,0 +1,13 @@
+package com.study.ex_spring_batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExSpringBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExSpringBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/ex_spring_batch/ExSpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.ex_spring_batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExSpringBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
